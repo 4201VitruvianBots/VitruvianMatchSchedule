36cf8cb54a102700276df929995643b9cd750518
Gavin P <gavinjpeeler@gmail.com>
2023-10-24 20:54:32 -0700

    Timer now updating :yipee:

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,20 +6,20 @@ function Timer({targetName, targetDate}: {
     targetName: String
     targetDate: Date
 }) {
-    const currentDate = new Date();
+    const [currentDate, setCurrentDate] = useState(new Date());
     const [deltaTime, setDeltaTime] = useState(targetDate.valueOf() - currentDate.valueOf());
     const [secondsLeft, setSecondsLeft] = useState(0);
     const [minutesLeft, setMinutesLeft] = useState(0);
     const [hoursLeft, setHoursLeft] = useState(0);
     
-    const updateTimer = useCallback(() => {
-        setDeltaTime(targetDate.valueOf() - currentDate.valueOf());
-        setSecondsLeft(Math.floor(deltaTime / 1000) % 60);
-        setMinutesLeft(Math.floor(deltaTime / 1000 / 60) % 60);
-        setHoursLeft(Math.floor(deltaTime / 1000 / 60 / 60) % 24);
-      }, []);
-    
-    useInterval(updateTimer, 1000);
+    useInterval(() => {
+        setCurrentDate(new Date());
+        const newDeltaTime = targetDate.valueOf() - currentDate.valueOf();
+        setDeltaTime(newDeltaTime);
+        setSecondsLeft(Math.floor(newDeltaTime / 1000) % 60);
+        setMinutesLeft(Math.floor(newDeltaTime / 1000 / 60) % 60);
+        setHoursLeft(Math.floor(newDeltaTime / 1000 / 60 / 60) % 24);
+      }, 1000);
     
     return <>
         <p>{targetName} at {format(targetDate, "h:mm a")}</p>
