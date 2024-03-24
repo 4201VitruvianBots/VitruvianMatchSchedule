ce279e48fe7660aab5f21040f146ab02b76e0c0d
Gavin P <gavinjpeeler@gmail.com>
2024-03-24 09:27:45 -0700

    Top bar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import './index.css';
+import { useState } from 'react';
+import 'react-material-symbols/rounded';
+import { MaterialSymbol } from 'react-material-symbols';
 
 function App() {
+    const [settingsOpen, setSettingsOpen] = useState(false);
     
     return (
         <>
-            <p className="text-2xl">This is my app</p>
+            {settingsOpen &&
+                <div className="flex flex-auto">
+                    <div className="w-1/2 h-screen bg-black bg-opacity-50 z-50" />
+                    <div className="bg-[#38761d] max-h-[10vh] px-5 flex flex-auto justify-end z-50">
+                        <button onClick={() => setSettingsOpen(!settingsOpen)}>
+                            <MaterialSymbol icon="settings" fill color="white" size={96}/>
+                        </button>
+                    </div>
+                    
+                </div>
+            }
+            
+            <div>
+                <div className="bg-[#38761d] min-h-[10vh] fixed top-0 w-full px-5 flex flex-auto justify-between -z-10">
+                    <img src="4201logo.png" width={100} />
+                    <button onClick={() => setSettingsOpen(!settingsOpen)}>
+                        <MaterialSymbol icon="settings" fill color="white" size={96}/>
+                    </button>
+                </div>
+                
+                <div className="-z-10">
+                    
+                </div>
+                
+                <div className="bg-[#38761d] min-h-[10vh] fixed bottom-0 w-full -z-10">
+                    
+                </div>
+            </div>
         </>
     );
 }
