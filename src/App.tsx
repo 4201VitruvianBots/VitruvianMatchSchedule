4317b2dd959fa943f7e06f9867c0ed74f7389075
Gavin P <gavinjpeeler@gmail.com>
2024-03-24 17:14:41 -0700

    Added refresh functionality, cleaned up tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import { useState } from 'react';
 import 'react-material-symbols/rounded';
 import { MaterialSymbol } from 'react-material-symbols';
 import EditableButton from './components/EditableButton';
+import * as dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
 
 function App() {
     const [settingsOpen, setSettingsOpen] = useState(false);
     const [delayMins, setDelayMins] = useState(0);
+    
+    const [lastRefresh, setLastRefresh] = useState(new Date());
+    const refreshMessage = dayjs(lastRefresh).fromNow();
+    
+    const refreshData = () => {
+        setLastRefresh(new Date());
+    }
     
     return (
         <>
@@ -14,7 +25,7 @@ function App() {
                 <div className="flex flex-auto">
                     <div className="w-1/2 h-screen bg-black bg-opacity-50 z-50" onClick={() => setSettingsOpen(!settingsOpen)}/>
                     <div className="block w-1/2">
-                        <div className="bg-[#38761d] max-h-[10vh] px-5 flex flex-auto justify-end z-50">
+                        <div className="bg-barGreen max-h-[10vh] px-5 flex flex-auto justify-end z-50">
                             <button onClick={() => setSettingsOpen(!settingsOpen)}>
                                 <MaterialSymbol icon="settings" fill color="white" size={96}/>
                             </button>
@@ -29,7 +40,7 @@ function App() {
             }
             
             <div>
-                <div className="bg-[#38761d] min-h-[10vh] fixed top-0 w-full px-5 flex flex-auto justify-between -z-10">
+                <div className="bg-barGreen min-h-[10vh] fixed top-0 w-full px-5 flex flex-auto justify-between -z-10">
                     <img src="4201logo.png" width={100} />
                     <button onClick={() => setSettingsOpen(!settingsOpen)}>
                         <MaterialSymbol icon="settings" fill color="white" size={96}/>
@@ -40,28 +51,28 @@ function App() {
                     
                 </div>
                 
-                <div className="bg-[#38761d] min-h-[10vh] max-h-[10vh] fixed bottom-0 w-full -z-10 block h">
+                <div className="bg-barGreen min-h-[10vh] max-h-[10vh] fixed bottom-0 w-full -z-10 block h">
                     <br />
                     <div className="flex px-5 justify-between">
                         <div className="flex flex-auto">
-                        <div className="bg-[#499529] max-w-[64px]">
-                            <button>
+                        <div className="bg-buttonOuterGreen max-w-[64px]">
+                            <button onClick={refreshData}>
                                 <MaterialSymbol icon="refresh" fill color="white" size={50}/>
                             </button>
                         </div>
-                        <p className="text-white text-3xl p-2">Refreshed 5 seconds ago</p>
+                        <p className="text-white text-3xl p-2">Refreshed {refreshMessage}</p>
                         </div>
                         
                         <div className="flex flex-auto">
-                        <div className="bg-[#499529] max-w-[64px]">
+                        <div className="bg-buttonOuterGreen max-w-[64px]">
                             <button onClick={() => setDelayMins(delayMins-1)}>
                                 <p className="text-white text-3xl p-2 min-w-10">-</p>
                             </button>
                         </div>
-                        <div className="bg-[#6aa84f] max-w-[64px]">
+                        <div className="bg-buttonInnerGreen max-w-[64px]">
                             <EditableButton value={delayMins} setValue={setDelayMins} />
                         </div>
-                        <div className="bg-[#499529] max-w-[64px]">
+                        <div className="bg-buttonOuterGreen max-w-[64px]">
                             <button onClick={() => setDelayMins(delayMins+1)}>
                                 <p className="text-white text-3xl p-2 min-w-10">+</p>
                             </button>
