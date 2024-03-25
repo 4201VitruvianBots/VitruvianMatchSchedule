5030827b8c2ebcceeed44193b49acf6e37a46a4f
Gavin P <gavinjpeeler@gmail.com>
2024-03-24 18:51:55 -0700

    Fixed some formatting

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,8 @@ function App() {
     return (
         <>
             {settingsOpen &&
-                <div className="flex flex-auto">
-                    <div className="w-1/2 h-screen bg-black bg-opacity-50 z-50" onClick={() => setSettingsOpen(!settingsOpen)}/>
+                <div className="flex fixed inset-0">
+                    <div className="w-1/2 h-screen bg-black bg-opacity-50" onClick={() => setSettingsOpen(!settingsOpen)}/>
                     <div className="block w-1/2">
                         <div className="bg-barGreen max-h-[10vh] px-5 flex flex-auto justify-end z-50">
                             <button onClick={() => setSettingsOpen(!settingsOpen)}>
@@ -53,15 +53,19 @@ function App() {
             }
             
             <div className="grid grid-rows-[auto_1fr_auto] h-screen">
-                <div className="bg-barGreen min-h-[10vh] w-full px-5 flex flex-auto justify-between -z-10">
+                <div className="bg-barGreen min-h-[10vh] w-full px-5 flex flex-auto justify-between">
                     <img src="4201logo.png" width={100} />
                     <button onClick={() => setSettingsOpen(!settingsOpen)}>
                         <MaterialSymbol icon="settings" fill color="white" size={96}/>
                     </button>
                 </div>
                 
-                <div className="-z-10 p-8 gap-8 grid grid-cols-[1fr_auto_1fr]">
-                    <div>
+                <div className="p-8 gap-8 grid grid-cols-[1fr_auto_1fr] min-h-0">
+                    <div className="space-y-5 overflow-y-auto">
+                        <Match matchInfo={testMatchInfo} teamNumber={4201}/>
+                        <p className="text-2xl">︙ 9 matches</p>
+                        <Match matchInfo={testMatchInfo} teamNumber={4201}/>
+                        <p className="text-2xl">︙ 4 matches</p>
                         <Match matchInfo={testMatchInfo} teamNumber={4201}/>
                     </div>
                     <div className="w-0.5 bg-gray-500" />
@@ -70,7 +74,7 @@ function App() {
                     </div>
                 </div>
                 
-                <div className="bg-barGreen min-h-[10vh] max-h-[10vh] w-full -z-10 block h">
+                <div className="bg-barGreen min-h-[10vh] max-h-[10vh] w-full block h">
                     <br />
                     <div className="px-5 grid grid-cols-2">
                         <div className="flex flex-auto">
@@ -99,7 +103,6 @@ function App() {
                             <p className="text-white text-3xl p-2">Delay (minutes)</p>
                         </div>
                     </div>
-                    <br />
                 </div>
             </div>
         </>
--- src/components/Match.tsx
+++ src/components/Match.tsx
@@ -18,32 +18,32 @@ function Match({matchInfo, teamNumber} : {matchInfo: MatchInfo, teamNumber: numb
                 <p>{matchInfo.matchName}</p>
             </div>
             <div>
-                <div className="bg-red-500 border border-white">
+                <div className="bg-red-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.red1 ? "font-bold" : ""}>{matchInfo.red1}</p>
                 </div>
-                <div className="bg-blue-500 border border-white">
+                <div className="bg-blue-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.blue1 ? "font-bold" : ""}>{matchInfo.blue1}</p>
                 </div>
             </div>
             <div>
-                <div className="bg-red-500 border border-white">
+                <div className="bg-red-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.red2 ? "font-bold" : ""}>{matchInfo.red2}</p>
                 </div>
-                <div className="bg-blue-500 border border-white">
+                <div className="bg-blue-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.blue2 ? "font-bold" : ""}>{matchInfo.blue2}</p>
                 </div>
             </div>
             <div>
-                <div className="bg-red-500 border border-white">
+                <div className="bg-red-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.red3 ? "font-bold" : ""}>{matchInfo.red3}</p>
                 </div>
-                <div className="bg-blue-500 border border-white">
+                <div className="bg-blue-500 border border-white h-full">
                     <p className={teamNumber == matchInfo.blue3 ? "font-bold" : ""}>{matchInfo.blue3}</p>
                 </div>
             </div>
             <div className="row-span-2 bg-gray-400">
                 {matchInfo.queue && <p>Q {matchInfo.queue.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>}
-                {matchInfo.matchStart && <p>Q {matchInfo.matchStart.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>}
+                {matchInfo.matchStart && <p>S {matchInfo.matchStart.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>}
             </div>
         </div>
     );
